92b0845cf08203bf7548e087e786bb02f7220dd8
benoansah2025 <memphisperla@gmail.com>
2025-10-24 11:14:44 +0000

    made changes to except handling statement to reflect alx checker

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector 
-from mysql.connector import Error
 
 def create_database(): 
     try:
@@ -15,7 +14,7 @@ def create_database():
             sql_statement = "CREATE DATABASE IF NOT EXISTS alx_book_store;"
             my_cursor.execute(sql_statement)
             print("'alx_book_store' created successfully!") 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
     finally:
         if mysql_db.is_connected():
